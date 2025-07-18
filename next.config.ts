cc2db0a046d8624c19dd4dcbeb3b65786fcacd4b
Enzo Patti <enzo.patti@aluno.senai.br>
2025-07-18 16:20:22 -0300

    dwa

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
  output: "export",
  reactStrictMode: true,
  trailingSlash: true,
+ basePath: isProd ? "/mottu-test-deploy-github-pages" : "",
+ assetPrefix: isProd ? "/mottu-test-deploy-github-pages/" : "",
  images: {
-   unoptimized: true,
+  unoptimized: true,
  },
- // Only add basePath if deploying to GitHub Pages under a repository name
- // For GitHub Pages user/org sites, leave this commented out
- basePath: "/mottu-test-deploy-github-pages",
 };
 
 export default nextConfig;
