596b9a9607054855c0ae53f6047c3ba197a31c0d
Enzo Patti <enzo.patti@aluno.senai.br>
2025-07-18 15:29:08 -0300

    github pages configured

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ basePath: "/2048-in-react",
+ output: "export",
+ reactStrictMode: true,
 };
 
 export default nextConfig;
